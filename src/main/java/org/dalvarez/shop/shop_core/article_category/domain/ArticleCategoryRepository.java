d373bf4c1fc254b0374139a3214e43f2f7d701fb
Domingo Álvarez <anthrax932yt@gmail.com>
2021-12-25 11:39:30 +0100

    Update article-category relationship #9

--- src/main/java/org/dalvarez/shop/shop_core/article_category/domain/ArticleCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dalvarez.shop.shop_core.article_category.domain;
-
-import org.dalvarez.shop.shop_common.persistence.domain.repository.GenericRepository;
-
-public interface ArticleCategoryRepository extends GenericRepository<ArticleCategory, String> {
-
-}
---
+++ src/main/resources/postgres/article.sql
@@ -0,0 +1,28 @@
+create sequence shop_core.article_seq
+    start with 100
+    increment by 100
+    cache 1;
+
+alter sequence shop_core.article_seq owner to shop_admin;
+grant all on shop_core.article_seq to shop_admin;
+--alter sequence article_seq restart;
+
+create table shop_core.article (
+    id bigint not null default nextval('shop_core.article_seq'),
+    description varchar(600) not null,
+    name varchar(255) not null,
+    price double precision not null,
+    stock bigint not null,
+    uuid varchar(36) not null unique,
+    created_at timestamp not null,
+    updated_at timestamp not null,
+    category_id bigint not null,
+    constraint article_pk primary key (id),
+    constraint article_category_fk foreign key (category_id) references shop_core.category(id)
+);
+
+alter table shop_core.article owner to shop_admin;
+grant all on table shop_core.article to shop_admin;
+
+--create unique index article_uuid_idx
+--    on shop.article using btree (uuid);
