abefb60dc195edd32e994ea1576c8c4926565c55
Domingo Álvarez <anthrax932yt@gmail.com>
2022-02-13 12:30:56 +0100

    Update script

--- makefile_scripts/help.sh
+++ makefile_scripts/help.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+green='\033[0;32m'
+orange='\033[0;33m'
+cyan='\033[0;36m'
+white='\033[1;37m'
+
 function print_deps_targets() {
 	local deps_targets_arr=("$@")
 
@@ -7,10 +12,10 @@ function print_deps_targets() {
 		return 0
 	fi
 
-	echo -ne "\n   ${WHITE}Depends on:"
+	echo -ne "\n   ${white}Depends on:"
 
 	for dep_target in "${deps_targets_arr[@]}"; do
-		echo -ne "\n    $WHITE-$CYAN make $dep_target"
+		echo -ne "\n    $white-$cyan make $dep_target"
 	done
 
 	echo
@@ -19,7 +24,7 @@ function print_deps_targets() {
 function print_targets() {
 	local makefile_path=$1
 
-	echo -ne "${WHITE}Help for $makefile_path\n"
+	echo -ne "${white}Help for $makefile_path\n"
 
 	local targets_str=`cat "$makefile_path" | egrep -i '^[a-zA-Z_-]+:.+##.+$' | tr '\n' ';'`
 	IFS=';' read -ra targets_arr <<< "$targets_str"
@@ -28,8 +33,8 @@ function print_targets() {
         	local target_name=$(echo "$target" | cut -d ":" -f 1)
         	local target_description=$(echo "$target" | cut -d "#" -f 3)
 
-        	echo -e "\n $WHITE-$GREEN$target_description"
-        	echo -e "  $ORANGE make $target_name"
+        	echo -e "\n $white-$green$target_description"
+        	echo -e "  $orange make $target_name"
 
        		local deps_targets_str=$(echo "$target" | cut -d ":" -f 2 | cut -d "#" -f 1)
         	IFS=' ' read -ra deps_targets_arr <<< "$deps_targets_str"
